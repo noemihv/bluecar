7a3607ff0741976492eb92a9cf366987334b799a
Omar Verduzco <orverduzco@gmail.com>
2017-04-04 01:07:18 -0500

    setup gulp tasks

--- gulp-tasks/wl-theme-tasks.js
+++ gulp-tasks/wl-theme-tasks.js
@@ -1,5 +1,5 @@
 /******************************************************************
-Wizeline PNG theme tasks
+Bluecar theme tasks
 
 Here are the gulp tasks to automatize transpiling of the theme files
 
@@ -17,21 +17,21 @@ var uglify = require('gulp-uglifycss');
 gulp.task('wl-theme-sass-dev', function () {
     
     //transpile style.scss to css
-    gulp.src('./wp-content/themes/wl_png/library/scss/style.scss')
+    gulp.src('./wp-content/themes/bones4/library/scss/style.scss')
         .pipe(sass())
-        .pipe(gulp.dest('./wp-content/themes/wl_png/library/css')) //write in prod folder
+        .pipe(gulp.dest('./wp-content/themes/bones3/library/css')) //write in prod folder
         .pipe(browserSync.stream()); //sync with browserSync
 
     //transpile style.scss to css
-    gulp.src('./wp-content/themes/wl_png/library/scss/ie.scss')
+    gulp.src('./wp-content/themes/bones3/library/scss/ie.scss')
         .pipe(sass())
-        .pipe(gulp.dest('./wp-content/themes/wl_png/library/css')) //write in prod folder
+        .pipe(gulp.dest('./wp-content/themes/bones3/library/css')) //write in prod folder
         .pipe(browserSync.stream()); //sync with browserSync
 
     //transpile style.scss to css
-    gulp.src('./wp-content/themes/wl_png/library/scss/login.scss')
+    gulp.src('./wp-content/themes/bones3/library/scss/login.scss')
         .pipe(sass())
-        .pipe(gulp.dest('./wp-content/themes/wl_png/library/css')) //write in prod folder
+        .pipe(gulp.dest('./wp-content/themes/bones3/library/css')) //write in prod folder
         .pipe(browserSync.stream()); //sync with browserSync
 
 });
@@ -39,7 +39,7 @@ gulp.task('wl-theme-sass-dev', function () {
 gulp.task('wl-theme-sass-watch', function () {
 
     //watch and transpile a theme files when they change
-    gulp.watch('./wp-content/themes/wl_png/library/scss/**/*.scss', ['wl-theme-sass-dev']);
+    gulp.watch('./wp-content/themes/bones3/library/scss/**/*.scss', ['wl-theme-sass-dev']);
 
 });
 
@@ -52,23 +52,23 @@ gulp.task('wl-theme-sass-watch', function () {
 gulp.task('wl-theme-sass', function () {
 
     //transpile scss to sass
-    gulp.src('./wp-content/themes/wl_png/library/scss/style.scss')
+    gulp.src('./wp-content/themes/bones3/library/scss/style.scss')
         .pipe(sass())
         .pipe(uglify()) //minimize        
-        .pipe(gulp.dest('./wp-content/themes/wl_png/library/css')); //write in prod folder
+        .pipe(gulp.dest('./wp-content/themes/bones3/library/css')); //write in prod folder
 
-    gulp.src('./wp-content/themes/wl_png/library/scss/ie.scss')
+    gulp.src('./wp-content/themes/bones3/library/scss/ie.scss')
         .pipe(sass())
         .pipe(uglify()) //minimize        
-        .pipe(gulp.dest('./wp-content/themes/wl_png/library/css')); //write in prod folder
+        .pipe(gulp.dest('./wp-content/themes/bones3/library/css')); //write in prod folder
 
-    gulp.src('./wp-content/themes/wl_png/library/scss/editor-style.scss')
+    gulp.src('./wp-content/themes/bones3/library/scss/editor-style.scss')
         .pipe(sass())
         .pipe(uglify()) //minimize        
-        .pipe(gulp.dest('./wp-content/themes/wl_png/library/css')); //write in prod folder
+        .pipe(gulp.dest('./wp-content/themes/bones3/library/css')); //write in prod folder
 
-    gulp.src('./wp-content/themes/wl_png/library/scss/login.scss')
+    gulp.src('./wp-content/themes/bones3/library/scss/login.scss')
         .pipe(sass())
         .pipe(uglify()) //minimize        
-        .pipe(gulp.dest('./wp-content/themes/wl_png/library/css')); //write in prod folder
+        .pipe(gulp.dest('./wp-content/themes/bones3/library/css')); //write in prod folder
 });
